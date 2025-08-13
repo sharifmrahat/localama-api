8a52283122a4d997710c3804759789ccec021071
sharifmrahat <sharifmrahat@gmail.com>
2025-08-13 22:35:12 +0600

    dockerfile is updated

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,7 @@ export default () => ({
   environment: process.env.ENVIRONMENT || 'Development',
   appName: process.env.APP_NAME || 'Localama API',
   hostUrl: process.env.HOST_URL || 'http://localhost',
-  port: parseInt(process.env.PORT || '5050', 10),
+  port: parseInt(process.env.PORT || '5000', 10),
   apiPrefix: process.env.API_PREFIX || '/api/v1',
   ollamaHost: process.env.OLLAMA_HOST || 'http://localhost:11434/api/chat',
 });
